b7d34f5949a1c416e5f94337379cb8749dbe7c31
Isaiah Sylvester <isaiah_tip@hotmail.com>
2020-12-01 09:43:17 -0500

    Updated about.js and added picutres

--- components/About/About.js
+++ components/About/About.js
@@ -1,16 +1,106 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 export default function About() {
   return (
-    <View>
-      <Text h1>Group Members</Text>
-      <Text>Alexander Koumarianos - 101075604</Text>
-      <Text>Anass Elabeidi - 101220896</Text>
-      <Text>Hyeonjun Yoon - 1012113</Text>
-      <Text>Isaiah Sylvester - 100859973</Text>
-      <Text>Jack Stamcos - 101225743</Text>
-      <Text> Vitalii Moudrak - 101230862</Text>
+    <>
+    <ScrollView>
+    <View style={styles.head}>
+      <Text style={styles.header}>The Team</Text>
     </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/clooney.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Alexander Koumarianos</Text>
+        <Text>Student ID: 101075604</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 25</Text>
+      </View> 
+    </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/person-1.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Anass Elabeidi</Text>
+        <Text>Student ID: 101220896</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 32</Text>
+      </View> 
+    </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/man.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Hyeonjun Yoon</Text>
+        <Text>Student ID: 1012113</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 26</Text>
+      </View> 
+    </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/tyson.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Isaiah Sylvester</Text>
+        <Text>Student ID: 100859973</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 25</Text>
+
+      </View> 
+    </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/woody.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Vitalii Moudrak</Text>
+        <Text>Student ID: 101230862</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 30</Text>
+      </View> 
+    </View>
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../../assets/miller.jpg")} />
+      <View style={styles.content}>
+        <Text style={styles.title}>Jack Stamcos</Text>
+        <Text>Student ID: 101225743</Text>
+        <Text>Country: Canada</Text>
+        <Text>Age: 18</Text>
+      </View> 
+    </View>
+    </ScrollView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    height: 100,
+    width: 370,
+    padding: 6,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    backgroundColor: "#eaeaea"
+  },
+  head: {
+    marginTop: 7,
+    padding: 11,
+    height: 60,
+    width: 370,
+  },
+  header: {
+    padding: 8,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  image: {
+    height: '100%',
+    flex: 0.35,
+  },
+  content: {
+    flex: 0.65,
+    paddingHorizontal: 11,
+  },
+  title: {
+    marginBottom: 6,
+    color: "#20232a",
+    fontSize: 15,
+    fontWeight: "bold"
+  }
+});
