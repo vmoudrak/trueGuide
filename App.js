ad716fd923bc75f9adfe780c964a92b1aba8c10a
Anass Elabeidi <Anasselabeidi5@gmail.com>
2020-11-30 22:35:01 -0500

    server connection

--- App.js
+++ App.js
@@ -31,6 +31,24 @@ export default function App() {
       </NavigationContainer>
   );
 }
+_storeData = async () => {
+  try {
+    await AsyncStorage.setItem('@MySuperStore:key', 'I like to save it.');
+  } catch (error) {
+    // Error saving data
+  }
+};
+_retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('TASKS');
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
